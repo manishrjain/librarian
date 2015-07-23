aed13b9b3c7bb005fc9f45641d686c0228b14b21
Manish R Jain <manishrjain@gmail.com>
2015-07-23 18:45:21 +1000

    handles moving images, removal of duplicates and extended filenames in case of collissions.

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"errors"
 	"flag"
 	"fmt"
 	"image"
@@ -9,6 +11,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"time"
 
 	"github.com/rwcarlsen/goexif/exif"
 
@@ -22,6 +25,44 @@ var dry = flag.Bool("dry", true, "Don't commit the changes."+
 	" Only show what would be performed")
 
 var dirs map[string]bool
+
+type State struct {
+	SrcPath string
+	Sum     []byte
+	Ext     string
+	Ts      time.Time
+}
+
+func (s *State) PathWithoutExtension(full bool) string {
+	dir := "Anarchs"
+	name := ""
+	if s.Ts.IsZero() {
+		if full {
+			name = fmt.Sprintf("%x", s.Sum)
+		} else {
+			name = fmt.Sprintf("%x", s.Sum[0:8])
+		}
+	} else {
+		dir = s.Ts.Format("2006Jan")
+		suffix := s.Sum[0:4]
+		if full {
+			suffix = s.Sum
+		}
+		name = fmt.Sprintf("%s_%x", s.Ts.Format("02_1504"), suffix)
+	}
+	folder := path.Join(*dst, dir)
+	return path.Join(folder, name)
+}
+
+func (s *State) ToPath() string {
+	path := s.PathWithoutExtension(false)
+	return path + "." + s.Ext
+}
+
+func (s *State) LongPath() string {
+	path := s.PathWithoutExtension(true)
+	return path + "." + s.Ext
+}
 
 func getType(f *os.File) (string, error) {
 	if _, err := f.Seek(0, 0); err != nil {
@@ -68,35 +109,58 @@ func dirExists(dir string) error {
 	return nil
 }
 
-func getFinalPath(f *os.File, ftype string, csum []byte) (string, error) {
+func getTimestamp(f *os.File) (rts time.Time, rerr error) {
 	if _, err := f.Seek(0, 0); err != nil {
-		return "", err
+		return rts, err
 	}
 
-	dfolder := "Anarchs"
-	fname := ""
 	x, err := exif.Decode(f)
 	if err == nil {
-		dt, ierr := x.DateTime()
-		if ierr == nil {
-			dfolder = dt.Format("2006Jan")
-			fname = fmt.Sprintf("%s_%x", dt.Format("02_1504"), csum[0:4])
+		if ts, ierr := x.DateTime(); ierr == nil {
+			return ts, nil
 		}
 	}
-	dfolder = path.Join(*dst, dfolder)
-	if err := dirExists(dfolder); err != nil {
-		fmt.Printf("Error checking/creating directory: %v\n", err)
-		return "", err
-	}
-
-	if fname == "" {
-		fname = fmt.Sprintf("%x", csum[0:8])
-	}
-	fname = fname + "." + ftype
-	return path.Join(dfolder, fname), nil
+	return rts, errors.New("Unable to find ts")
 }
 
-func moveFile(src, dst string) {
+func moveFile(state State) error {
+	pattern := state.PathWithoutExtension(false) + "*"
+	fmt.Printf("Pattern: %v\n", pattern)
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
+	fmt.Println(matches)
+	if len(matches) == 0 {
+		fmt.Printf("Moving %s to %s\n", state.SrcPath, state.ToPath())
+		if *dry {
+			return nil
+		}
+		return os.Rename(state.SrcPath, state.ToPath())
+	}
+
+	for _, dup := range matches {
+		f, err := os.Open(dup)
+		if err != nil {
+			return err
+		}
+		dupsum, err := getSum(f)
+		if err != nil {
+			return err
+		}
+		if bytes.Equal(state.Sum, dupsum) {
+			// src is a duplicate of a file which already is copied to destination.
+			fmt.Printf("Already exists: %s. Deleting %s", dup, state.SrcPath)
+			if *dry {
+				return nil
+			}
+			return os.Remove(state.SrcPath)
+		}
+	}
+
+	// Doesn't match with any of the existing files.
+	// Let's move this image
+	return nil
 }
 
 func handleFile(path string) error {
@@ -107,26 +171,24 @@ func handleFile(path string) error {
 	}
 	defer f.Close()
 
-	t, err := getType(f)
-	if err != nil {
+	var state State
+	state.SrcPath = path
+	if state.Ext, err = getType(f); err != nil {
 		fmt.Println("Not an image file. Moving on...")
 		return nil
 	}
 
-	csum, err := getSum(f)
-	if err != nil {
+	if state.Sum, err = getSum(f); err != nil {
 		return err
 	}
 
-	fpath, err := getFinalPath(f, t, csum)
-	if err != nil {
-		return err
+	if state.Ts, err = getTimestamp(f); err != nil {
+		state.Ts = time.Time{}
 	}
 
 	// We already have the folder as the YYYYMMM,
 	// so no need to have that part in the file name.
-	fmt.Println("Final path: " + fpath)
-	return nil
+	return moveFile(state)
 }
 
 func walkFn(path string, info os.FileInfo, err error) error {
